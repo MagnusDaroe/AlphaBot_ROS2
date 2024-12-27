c668589c0601405b512110ebafad6bffb1163cb8
MagnusDaroe <116544128+MagnusDaroe@users.noreply.github.com>
2024-12-27 15:02:59 +0100

    remove servo

--- src/alpha_bot/scripts/alphabot_node.py
+++ src/alpha_bot/scripts/alphabot_node.py
@@ -42,10 +42,10 @@ class AlphaBotNode(Node):
         self.PWMB.start(50)
         
         # Initialize PWM for servos
-        self.J1 = GPIO.PWM(self.S1, 50)
-        self.J2 = GPIO.PWM(self.S2, 50)
-        self.J1.start(3)
-        self.J1.start(3)
+        #self.J1 = GPIO.PWM(self.S1, 50)
+        #self.J2 = GPIO.PWM(self.S2, 50)
+        #self.J1.start(3)
+        #self.J1.start(3)
 
         # Initialize GPIO pins for sensors
         self.DR = 16  # Right sensor
@@ -74,7 +74,7 @@ class AlphaBotNode(Node):
         angular_z = msg.angular.z
         
         # Set the angle of the servos
-        self.setAngle(50, 50)
+        #self.setAngle(50, 50)
 
         # Calculate motor speeds based on linear and angular velocities
         left_speed, right_speed = self.calculate_speeds(linear_x, angular_z)   
@@ -151,8 +151,8 @@ class AlphaBotNode(Node):
         """Set the angle of the servos."""
         # Set PWM duty cycles (absolute value of speed)
         
-        j1 = 0
-        j2 = 0
+        j1 = 3
+        j2 = 3
         
         self.PWMA.ChangeDutyCycle(abs(j1))
         self.PWMB.ChangeDutyCycle(abs(j2))
